2e57544933d456dcf3b86ccb9e63c4d4662e847a
jorge.shortys@gmail.com <jorge.shortys@gmail.com@8c5b86f6-381a-a5f1-69c3-6d1201426c32>
2015-01-03 17:25:43 +0000

    Example8_1

---
+++ Example8_1/src/Example8_1.java
@@ -0,0 +1,34 @@
+import java.net.*;
+import java.io.*;
+
+/*
+ * Example 8-1. A daytime protocol client
+ */
+public class Example8_1 {
+	public static void main(String[] args) {
+		String hostname = args.length > 0 ? args[0] : "time.nist.gov";
+		System.out.println(hostname);
+		Socket socket = null;
+		try {
+			socket = new Socket(hostname, 13);
+			socket.setSoTimeout(15000);
+			InputStream in = socket.getInputStream();
+			StringBuilder time = new StringBuilder();
+			InputStreamReader reader = new InputStreamReader(in, "ASCII");
+			for (int c = reader.read(); c != -1; c = reader.read()) {
+				time.append((char) c);
+			}
+			System.out.println(time);
+		} catch (IOException ex) {
+			System.err.println(ex);
+		} finally {
+			if (socket != null) {
+				try {
+					socket.close();
+				} catch (IOException ex) {
+					// ignore
+				}
+			}
+		}
+	}
+}
